9238ac4853b8d6195a4de79ddcbdfe13f4253f31
bosseman <k.bosseman@hotmail.com>
2024-02-06 16:41:22 +0100

    exo 6.8 fini

--- JavaScript/algobox_js/exo6.8/scripts/main.js
+++ JavaScript/algobox_js/exo6.8/scripts/main.js
@@ -4,14 +4,15 @@ nb_neg = 0;
 nb_pos = 0;
 
 for (let i = 0; i < nbre; i++) {
+    // je demande à l'utilisateur de rentrer ses valeurs dans ma boucle
     tableau[i] = parseInt(prompt("saisissez vos valeurs"));
-    if (i < 0) {
-        nb_neg++;
-    else (i > 0){
-        nb_pos++;
+    // 
+    if (tableau[i] < 0) {
+        nb_neg+=1;
+    }
+    else {
+        nb_pos+=1;
         }
     }
-}
-alert("il y a " + nb + " nombres négatif\n il y a " + nb_pos + "");
-
+alert("il y a " + nb_neg + " nombres négatif\n il y a " + nb_pos + "nombres positifs");
 
