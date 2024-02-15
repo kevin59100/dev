1aa6468937ef9f1793f06cd0536b461430e725dc
bosseman <k.bosseman@hotmail.com>
2024-02-15 15:51:00 +0100

    suite exo DOM

--- dom/Apprendre à modifier le DOM en JS/dom.js
+++ dom/Apprendre à modifier le DOM en JS/dom.js
@@ -1,48 +1,79 @@
 // afficher le texte de la balise ayant l'id 'titre2'
-let title2= document.getElementById("titre2");
-let dom=document.getElementById("dom");
+let title2 = document.getElementById("titre2");
+let dom = document.getElementById("dom");
 // afficher le nombre de balises ayant la classe 'titre'
 // le querySelectorAll fera appel à un tableau 
-let nbreBalise=document.querySelectorAll('.titre').length;
+let nbreBalise = document.querySelectorAll('.titre').length;
 // afficher le texte de toutes les balises ayant la classe 'titre'
 // getElementsByClassName fait appel à une classe et renvoie un tableau
 let totTitle = document.getElementsByClassName('titre')
 // afficher le texte de toutes les balises 'li'
 let listText = document.querySelectorAll('.liste > li');
 
-// lire le bouton dom
-dom.addEventListener('click', function(){
-    // afficher le texte de la balise ayant l'id 'titre2'
-    alert(title2.innerText);
-    // afficher le nombre de balises ayant la classe 'titre'
-    alert(nbreBalise);
-    // afficher le texte de toutes les balises ayant la classe 'titre'
-  for(i=0; i < totTitle.length; i++){
+// LIRE LE DOM
+
+dom.addEventListener('click', function () {
+  // afficher le texte de la balise ayant l'id 'titre2'
+  alert(title2.innerText);
+  // afficher le nombre de balises ayant la classe 'titre'
+  alert(nbreBalise);
+  // afficher le texte de toutes les balises ayant la classe 'titre'
+  for (i = 0; i < totTitle.length; i++) {
     alert(totTitle[i].innerText);
   }
   // afficher le texte de toutes les balises 'li'
-  for(i=0; i < listText.length; i++){
+  for (i = 0; i < listText.length; i++) {
     alert(listText[i].innerText);
   }
 })
 
 // =============================================
 
+// MODIFIER LE DOM EN JS
+
 let modifDom = document.getElementById("dom_modif");
 let title1 = document.getElementById('titre1');
 let para = document.getElementById('paragraphe1')
 
 modifDom.addEventListener('click', function () {
-    // manipulation des contenus
-    // Si contenu HTML est attribué à innerHTML, le contenu HTML existant est remplacé par le nouveau.
-    // changer le contenu du h1 en 'Bienvenue'
-    title1.innerHTML = "<h1>bienvenue</h1>";
-    // changer le contenu du h2 en 'Exo JS'
-    title2.innerHTML = "<h2>Exo JS</h2>";
-    // changer le contenu paragraphe en 'Voici quelques fonctions exécutées en javascript.'
-    para.innerHTML = "<p>'Voici quelques fonctions exécutées en javascript.'</p>";
-    
-   
+  // manipulation des contenus
+  // Si contenu HTML est attribué à innerHTML, le contenu HTML existant est remplacé par le nouveau.
+  // changer le contenu du h1 en 'Bienvenue'
+  title1.innerHTML = "<h1>bienvenue</h1>";
+  // changer le contenu du h2 en 'Exo JS'
+  title2.innerHTML = "<h2>Exo JS</h2>";
+  // changer le contenu paragraphe en 'Voici quelques fonctions exécutées en javascript.'
+  para.innerHTML = "<p>'Voici quelques fonctions exécutées en javascript.'</p>";
+  // changer le contenu de chaque point (li) de la liste en "Liste 1", "Liste 2", etc...
+  for (i = 0; i < listText.length; i++) {
+    // Similaire à innerText, la fonction innerHTML permet de récupérer ou de définir le contenu HTML
+    // d'un élément.
+    listText[i].innerHTML = "<li>liste " + listText[i].innerHTML + "</li>";
+  }
+})
+
+// =============================================
+
+// MODIFIER LE STYLE EN JS
+
+let style = document.getElementById("style");
+let list = document.getElementById("liste")
+
+style.addEventListener('click', function () {
+  // aligner le titre 1 au centre de la page
+  title1.style.textAlign = 'center';
+  // mettre en rouge tous les éléments ayant la classe 'titre'
+  for (let i = 0; i < totTitle.length; i++) {
+    totTitle[i].style.color = 'red';
+    // mettre une bordure 'solid'
+    para.style.borderStyle = 'solid';
+    // mettre une bordure 'solid 1px'
+    para.style.borderWidth = '1px';
+    // mettre un padding '10px' au paragraphe
+    para.style.padding = '10px';
+    // faire disparaître la liste
+    list.remove("liste");
+  }
 })
 
 
--- dom/formulaire/asset/js/main.js
+++ dom/formulaire/asset/js/main.js
@@ -1,3 +1,31 @@
-document.getElementById("form1").addEventListener("submit", function(){
-    alert("vous avez saisi : "+pseudo.value+"");
+let society = document.getElementById('pseudo');
+let contact = document.getElementById('contact');
+let post = document.getElementById('post');
+let town = document.getElementById('town');
+let mail = document.getElementById('mail');
+let button = document.getElementById('send');
+let title = document.getElementById('title');
+
+title.style.color='red';
+
+
+button.addEventListener('click', function(){
+    if(society.value < 1){
+        alert('saisir au moins 1 caractère');
+    }
+    if(contact.value < 1){
+        alert('saisir au moins 1 caractère');
+    }
+    if(post.value.length != 5){
+        alert('saisir 5 caratères numériques');
+    }
+    if(town.value < 1){
+        alert('saisir au moins 1 caractère');
+    }
+    // if(mail.value.includes('@')==false){
+        if(!mail.value.includes('@')){
+        alert('saisir au moins @');
+    }
 })
+
+
